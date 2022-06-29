be1bbf76ce75e528af4a54fef28fa01ee157f2d4
Chris Kade <chriskade1999@gmail.com>
2022-06-29 00:09:57 -0400

    Added context

---
+++ client/src/setup/app-context-manager/index.js
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+const initialState = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    sessionToken: "",
+    signedIn: false
+}
+
+
+export const UserProvider = ({children}) => {
+    const [state, setState] = useState(initialState);
+
+
+
+    return (
+        <UserContext.Provider value={[state, setState]}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export const UserConsumer = UserContext.Consumer;
+
+export default UserContext;
