b64e1be4d15f4fe9c32b09750d42d7c9b14746a2
Chris Kade <chriskade1999@gmail.com>
2022-07-04 02:12:39 -0400

    Broken create poll page.

--- client/src/common/footer/index.js
+++ client/src/common/footer/index.js
@@ -10,7 +10,7 @@ const Footer = (props) => {
             <p className="text-xl text-black font-bold font-mono p-1 mt-2">&copy; EasyPolls  {(new Date().getFullYear())}</p>
             <p className="font-mono text-lg font-bold text-gray-600  mb-1">Made by Chris Kade</p>
         </div> 
-        <div className="flex place-content-evenly mt-1 w-1/3 ">
+        <div className="flex place-content-evenly mt-1 w-1/3 place-items-center">
             <a href="https://www.linkedin.com/in/christopher-kade-b9b2151a5/" className="soc-link text-blue-400 not-italic font-bold text-2xl rounded-full h-10 w-10 text-center leading-8"><img src={linkedin}/></a>
             <a href="https://twitter.com/Christhedev__" className="soc-link h-10 w-10 flex justify-center"><img src={twitter} className="w-full" alt="twitter icon" /></a>
             <a href="https://www.github.com/ckade15" className="git-link h-10 w-10 flex justify-center"><img src={github} alt="github icon" className="w-full h-full"/></a>
--- client/src/pages/create-poll/index.js
+++ client/src/pages/create-poll/index.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 import Header from "../../common/header";
 import Footer from '../../common/footer';
+import UserContext from "../../setup/app-context-manager";
+import { checkToken } from "../../setup/auth";
 
 const CreatePoll = (props) => {
+    const [context, setContext] = useContext(UserContext);
+    useEffect(() => {
+        document.title = 'EasyPolls - Create a Poll';
+        alreadyLoggedIn();
+    });
+
+    const alreadyLoggedIn = () => {
+        const token = localStorage.getItem('sessionToken');
+        try{
+            if (token.length > 0){
+                const validToken = checkToken(token).then(token => {
+                    setContext({
+                        ...context,
+                        firstName: token.data.firstName,
+                        lastName: token.data.lastName,
+                        email: token.data.email,
+                        sessionToken: token.data.sessionToken,
+                        signedIn: true,
+                        id: token.data.id
+                    })
+                });
+            }else{
+                
+            }
+    
+        }catch{
+            
+        }
+    }
+
     return (
-        <section></section>
+        <React.Fragment>
+            {context.signedIn ? <></> : <Navigate to='/login'/>}
+            <Header page="Create Poll" />
+            <Footer />
+        </React.Fragment>
     );
 }
 
