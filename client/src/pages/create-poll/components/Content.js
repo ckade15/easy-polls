3007a0858529908c1b27a05e806c321b44e4a617
Chris Kade <chriskade1999@gmail.com>
2022-07-10 21:07:35 -0400

    Working form on create poll page with dynamic list rendering to add poll items.

--- client/src/pages/create-poll/components/Content.js
+++ client/src/pages/create-poll/components/Content.js
@@ -1,6 +1,7 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import UserContext from '../../../setup/app-context-manager';
 import addCircle from '../../../assets/add_circle.svg';
+import cancel from '../../../assets/cancel.svg'
 
 const Content = () => {
     const [context, setContext] = useContext(UserContext);
@@ -12,8 +13,13 @@ const Content = () => {
     const handleAdd = () => {
         state.items.push({
             name: ""
-        })
-    }
+        });
+        setState({...state})
+    };
+
+    useEffect((e) => {
+
+    }, [state, state.items])
 
     return (
         <main className='w-full h-fit bg-[#AF4D98] pt-10 pb-10 min-h-screen font-mono'>
@@ -27,13 +33,30 @@ const Content = () => {
                     <p className='mr-6'>Click here to add a poll item</p>
                     <a className="hover:cursor-pointer h-fit w-fit " ><img src={addCircle} className="" onClick={handleAdd}/></a>
                 </div>
-                {state.items.length > 0 ? state.items.map(item =>{
-                    <div>
-                        <p>Entry name:</p>
-                        <input type='text' />
-                        
-                    </div>
-                }) : <></>}
+                {state.items.map((item, index) =>{
+                    const handleInput = (e) => {
+                        /*
+                        let items  = [...state.items];
+                        let i = {...items[index]};
+                        i.name = e.target.value;
+                        items[index] = i;
+                        setState({...state, items});*/
+                        state.items[index] = e.target.value;
+                    }
+
+                    const handleCancel = () => {
+                        state.items.pop(index)
+                        setState({...state})
+                    }
+
+                    return (
+                        <div key={index} className='flex mt-4 w-1/2 ml-auto mr-auto place-items-center'>
+                            <p>Entry name:</p>
+                            <input type='text' key={item.name} onChange={e => {handleInput(e)}} className='ml-8 rounded-md p-1 shadow-md h-fit'/>
+                            <a className='ml-8 hover:cursor-pointer'><img src={cancel} onClick={handleCancel} /></a>
+                        </div>
+                    )
+                })}
             </section>
         </main>
     );
