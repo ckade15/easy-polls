2f5d442161d040d6f220851cfcce68052a7fedb3
Chris Kade <chriskade1999@gmail.com>
2022-07-19 22:09:22 -0400

    Added max and min height to poll components on home page

--- client/src/pages/main/components/Poll.js
+++ client/src/pages/main/components/Poll.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Poll = (props) => {
   const URL = 'http://localhost:3000/poll/' + props.pollId;
   return (
-    <div className='bg-blue-400 w-fit p-10 font-mono flex-col justify-center text-center rounded-lg shadow-lg'>
+    <div className='bg-blue-400 w-fit p-10 font-mono flex-col justify-center text-center rounded-lg shadow-lg min-h-[250px] max-h-[250px]'>
       <p className='text-xl'>Poll title: {props.title}</p>
       <p className='mt-4 mb-8 text-lg'>Creator: {props.createdBy}</p>
       <a href={URL} className='text-lg bg-[#F4E4BA] font-bold p-4 rounded-md text-gray-500 hover:shadow-lg hover:text-[#F4E4BA] hover:bg-[#AF4D98] '>View Poll</a>
